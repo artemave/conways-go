a12eef38aa7e52cf2c8a413520dab91926d6f272
artemave <artemave@gmail.com>
2013-09-03 23:03:32 +0100

    more test code

--- game/game_test.go
+++ game/game_test.go
@@ -2,6 +2,7 @@ package game
 
 import (
   "testing"
+  "sort"
 )
 
 var GliderSeed = Generation{
@@ -11,6 +12,42 @@ var GliderSeed = Generation{
   &Cell{Row: 1, Col: 4},
   &Cell{Row: 0, Col: 3},
 }
+
+type ByGeneration Generation
+
+func (g ByGeneration) Len() int {
+  return len(g)
+}
+func (g ByGeneration) Swap(i, j int) {
+    g[i], g[j] = g[j], g[i]
+}
+func (g ByGeneration) Less(i, j int) bool {
+  if g[i].Row == g[j].Row {
+    return g[i].Col < g[j].Col
+  }
+  return g[i].Row < g[j].Row
+}
+
+
+func (this Generation) equal(other Generation) bool {
+  if len(this) != len(other) {
+    return false
+  }
+
+  sorted_this := this
+  sorted_other := other
+
+  sort.Sort(ByGeneration(sorted_this))
+  sort.Sort(ByGeneration(sorted_other))
+
+  for i, cell := range sorted_this {
+    if cell != sorted_other[i] {
+      return false
+    }
+  }
+  return true
+}
+
 
 func TestGliderGen1(t *testing.T) {
 
@@ -26,10 +63,8 @@ func TestGliderGen1(t *testing.T) {
 
   ActualGen := game.NextGeneration(&GliderSeed)
 
-  AssertActualGenMatchesExpected(t, ActualGen, &ExpectedGen)
-}
-
-func AssertActualGenMatchesExpected(t *testing.T, ActualGen *Generation, ExpectedGen *Generation) {
-  t.Errorf("Boom!")
+  if !ActualGen.equal(ExpectedGen) {
+    t.Errorf("Boom!")
+  }
 }
 
