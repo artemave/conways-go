fd74716b73b72b2a7087cbc94a162bbf6f3ee0e9
artemave <artemave@gmail.com>
2015-02-10 16:37:39 +0000

    always recover from panic

--- conways.go
+++ conways.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 
 	"github.com/araddon/gou"
 )
 
 func main() {
+	defer RecoverAndLogError()
+
 	gou.SetLogger(log.New(os.Stderr, "", log.LstdFlags), "debug")
 
 	RegisterRoutes()
@@ -20,4 +23,12 @@ func main() {
 
 	gou.Debug("listening at " + port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func RecoverAndLogError() {
+	if err := recover(); err != nil {
+		out := []byte{}
+		runtime.Stack(out, true)
+		gou.Error(string(out))
+	}
 }
